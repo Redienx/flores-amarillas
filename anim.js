a6c884b42fa3134b19153b0eea988238e92462bc
Rediex <snrideriglesias5@gmail.com>
2025-09-20 23:23:35 -0500

    mel

--- anim.js
+++ anim.js
@@ -1,27 +1,3 @@
 // Sincronizar las letras con la canción
 var audio = document.querySelector("audio");
 
-// Animar las letras
-function updateLyrics() {
-  var time = Math.floor(audio.currentTime);
-  var currentLine = lyricsData.find(
-    (line) => time >= line.time && time < line.time + 6
-  );
-
-  if (currentLine) {
-    // Calcula la opacidad basada en el tiempo en la línea actual
-    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
-    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
-
-    // Aplica el efecto de aparición
-    lyrics.style.opacity = opacity;
-    lyrics.innerHTML = currentLine.text;
-  } else {
-    // Restablece la opacidad y el contenido si no hay una línea actual
-    lyrics.style.opacity = 0;
-    lyrics.innerHTML = "";
-  }
-}
-
-setInterval(updateLyrics, 500);
-
